4ed439d4dcf68697547425d05c67b1e409ae51ae
Nunny R <nunnyreyes@gmail.com>
2021-02-17 13:23:35 -0500

    solved

--- array_problems/removeDuplicates.js
+++ array_problems/removeDuplicates.js
@@ -1,3 +1,25 @@
+
 var removeDuplicates = function(nums) {
-    //leetcode
+    //modify the array in place, array is already sorted
+    //remove the duplicates in place
+    //we are returning unique values, we don't care about repeating values after the     //last element
+    //have two pointers
+    
+    
+    for(let num in nums) {
+        //if there is no array take that into account
+        if(nums.length === 0){
+            return 0
+        } 
+        let i = 0
+        for(let j = 1; j < nums.length; j++){
+            //compare the elements at pointers
+            if(nums[i] !== nums[j]){
+                i++
+                //replace the element with the element my second pointer is showing
+                nums[i] = nums[j]
+            }
+        }  
+        return i + 1
+    }
 };
