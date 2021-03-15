51249ce23a856b9c39d7db98411ba451436dd6ce
Nunny R <nunnyreyes@gmail.com>
2021-03-15 15:02:41 -0400

    calc

---
+++ hackerRank/warmUp/calc.js
@@ -0,0 +1,123 @@
+want to write a function called 'evaluate'
+
+take a string, like "4+3-2-5-5*4"
+                             ^20
+                      ^7
+                      7-2-5-20
+                      
+
+"5*5*5*5*5"
+"5*5*5*5+"
+
+
+
+
+
+O(2*n) => O(n)
+while (not end of string)
+if (+)
+else if (-()
+else if (* or /) {
+//consume characters until + or -
+}
+}
+
+while (not end of evalArry) 
+{sum them up}
+
+
+O(n*n)=>O(n^2)
+
+while (not end of string) {
+// advance pointer to next character
+newstring=input_string
+while (not end of new_string) {
+// advance pointer to next character in newstring
+}
+
+}
+
+
+currSign = -1
+
+currValue = 5
+
+op = *
+
+nextVal = 4
+
+while (not at end of string and next op not + or -)
+if (op=='*') 
+currValue *= nextVal
+else if (op=='/')
+currValue /= nextVal
+else if (op=='+' || op == '-' || at end of string ) {
+arrEval.push(currSign * currValue)
+
+// currSign = (op=='+') 1
+
+// done with expression
+
+}
+
+[4, 3, -2, -5, -20]
+
+
+(5*5)+(3*3)-(2*2)+1-1
+
+let arrGiven = [5, * , 5 ...]
+
+
+
+
+let arrEval = [25, 9, 4, 1, 1]
+
+let finalArr += []
+
+if(*){
+
+}
+
+if(-){
+  finalArr -= arrEval[i]
+}
+
+
+
+
+
+(2*1*3*2*1*3*1*1)+5+5+(5*4*4*4)
+
+(x * y * z) / (a * b * c) -
+
+
+(1 * 2 / 3) + (4 / 5)
+
+
+
+return a integer, the answer
+
+in the case of "4+4", it will return 8
+
+"1" -> 1
+
+"1-1" -> 0
+
+
+-only single digit number in the string
+-ony +, -, *, / operators (no parens, no exponents, etc)
+-only valid input
+-no divide by 0
+
+
+
+arr += value
+
+
+
+
+
+[4,+,3,-,2....]
+
+
+
